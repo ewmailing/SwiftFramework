75bb961b3688cfb0c9925144631edf5a1bc8fa3c
Eric Wing <ewing -dot- public |-at-| gmail.com>
2015-06-25 06:15:30 -0700

    Initial Commit

---
+++ SwiftFramework/SwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFramework.h
+//  SwiftFramework
+//
+//  Created by Eric Wing on 6/25/15.
+//  Copyright (c) 2015 PlayControl Software LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftFramework.
+FOUNDATION_EXPORT double SwiftFrameworkVersionNumber;
+
+//! Project version string for SwiftFramework.
+FOUNDATION_EXPORT const unsigned char SwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFramework/PublicHeader.h>
+
+
